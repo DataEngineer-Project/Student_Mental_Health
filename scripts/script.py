cbce421f3c77ffa8019171e99cfa88ecb413b733
epiphanie8 <epiphanieatta8@gmail.com>
2024-11-02 20:10:16 +0000

    first commit

---
+++ scripts/script.py
@@ -0,0 +1,22 @@
+# Script ingestion et nettoyage de donnes
+import pandas as pd
+
+# chemin du fichier csv
+file_path = '../data/Student_Mental_health.csv'
+
+# Lire le fichier CSV dans un DataFrame
+df = pd.read_csv(file_path)
+
+# Nettoyage : Exemple de suppression des valeurs manquantes
+df = df.dropna()
+
+# Renomer mes colonnes
+df.columns = ['Timestamp','Sexe','Age','Profession','AnneeEtudeActuelle','CGPA','SituationMatrimonial','Depressif?','Anxieux?','CrisePanique?','SuivieParUnSpecialiste?']
+
+# Sauvegarde du fichier transformé dans le dossier output
+output_path= '../output/Student_Mental_health_nettoye.csv'
+df.to_csv(output_path)
+
+# Affiche les premières lignes pour vérifier l'ingestion
+print("Données initiales")
+print(df.head())
