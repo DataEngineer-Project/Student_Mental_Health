e47779aca9aefc0dfb57c45129544d8c0e68aad7
epiphanie8 <epiphanieatta8@gmail.com>
2024-11-03 07:38:57 +0000

    Data grouped and count  by sex and by age

--- scripts/script.py
+++ scripts/script.py
@@ -1,6 +1,7 @@
 # Script ingestion et nettoyage de donnes
-import pandas as pd
+from operator import index
 
+import pandas as pd
 # chemin du fichier csv
 file_path = '../data/Student_Mental_health.csv'
 
@@ -13,10 +14,26 @@ df = df.dropna()
 # Renomer mes colonnes
 df.columns = ['Timestamp','Sexe','Age','Profession','AnneeEtudeActuelle','CGPA','SituationMatrimonial','Depressif?','Anxieux?','CrisePanique?','SuivieParUnSpecialiste?']
 
-# Sauvegarde du fichier transformé dans le dossier output
-output_path= '../output/Student_Mental_health_nettoye.csv'
-df.to_csv(output_path, index= False)
+
+# nouvelle DataFrame
+df_restreind = df[['Sexe', 'Age']]
+# ajout de nouvelle colone
+
+# regrouper pas sexe et age et compter le nombre d'occurence
+df_restreind = df_restreind.groupby(['Sexe','Age']).size().reset_index(name='count')
+
+# chemin de sortir
+output_path= '../output/Student_Mental_health_nettoye.xlsx'
+# Sauvegarder dans un fichier Excel avec plusieurs feuilles
+# xls = pd.ExcelFile(output_path)
+# df1 = df.read_excel(xls,'first_Date_Clear', index=False)
+# df2 = df_restreind.read_excel(xls,'Count_By_Age_Sex', index=False)
+
+with pd.ExcelWriter(output_path) as writer:
+    df.to_excel(writer, sheet_name='first_Date_Clear', index=False)
+    df_restreind.to_excel(writer, sheet_name='Count_By_Age_Sex', index=False)
 
 # Affiche les premières lignes pour vérifier l'ingestion
 print("Données initiales")
 print(df.head())
+print(df_restreind)
